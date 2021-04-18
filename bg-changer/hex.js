801674668a723a8c8ae79516056e34c0e91f40d7
Amilkar Munoz <yellowballpython@gmail.com>
2021-04-18 00:34:12 -0600

    Fun stuff added :)

--- bg-changer/hex.js
+++ bg-changer/hex.js
@@ -1,12 +1,9 @@
-"use strict";
-
+const hexValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
 const btn = document.querySelector("#btnRandom")
 const bod = document.querySelector("#bod")
 const bgName = document.querySelector("#bgName")
 const colors = ["aqua","plum", "rebeccapurple", "cadetblue", "burlywood", "indianred", "dodgerblue", "coral", "chocolate", "navajowhite","cornflowerblue", "red","blue", "white", "crimson","gold","forestgreen","purple","cyan","lemonchiffon","yellow"]
 const letters = document.querySelectorAll("#letter")
-
-var visited;
 
 const randomLetterColors = () => {
     let colored;
@@ -21,17 +18,17 @@ const randomLetterColors = () => {
     })
 }
 const randomBackgroundColor = () => {
-    let randomColor = Math.floor(Math.random() * colors.length)
-    while (randomColor === visited) {
-        randomColor = Math.floor(Math.random() * colors.length)
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        let randomValue = Math.floor(Math.random() * hexValues.length);
+        color = color + hexValues[randomValue];
     }
-    visited = randomColor
-    let color = colors[randomColor]
+
     bod.style.background = color
-    bgName.innerHTML = `${color}`.replace(/^\w/, e => e.toUpperCase())
+    bgName.innerHTML = color
 }
 
 btn.addEventListener("click", () => {
-    randomLetterColors()
     randomBackgroundColor()
+    randomLetterColors()
 })
