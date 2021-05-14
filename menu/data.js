4696984a158f4db97bcffad01f77ee3d6d26abea
Amilkar Munoz <yellowballpython@gmail.com>
2021-05-14 08:28:50 -0600

    filtering menu items

---
+++ menu/data.js
@@ -0,0 +1,109 @@
+const food = [{
+    id: 1,
+    name: "La Magnifica",
+    desc: "Tres panes con torta 100% hecha a la parrilla, pepinillos, cebolla y salsas.",
+    type: "ham",
+    img: './images/ham1.jpg'
+},
+{
+    id: 2,
+    name: "La Tortola",
+    desc: "Con jalapeño, torta 100% hecha a la parrilla, pepinillos, cebolla y salsas. Deliciosa.",
+    type: "ham",
+    img: './images/ham3.jpg'
+},
+{
+    id: 3,
+    name: "La Lio",
+    desc: "Queso pepperjack, lechuga, tomate, hasbrown en pan artesanal de yuca. Torta a la parrilla.",
+    type: "ham",
+    img: './images/ham5.jpg'
+},
+{
+    id: 4,
+    name: "La Buscapleitos",
+    desc: "Tres tortas, pepinillos, cebolla, salsa BBQ",
+    type: "ham",
+    img: './images/ham4.jpg'
+},
+{
+    id: 5,
+    name: "La FIFAS",
+    desc: "Pan de la casa, tomate, mayonesa, pechuga de pollo.",
+    type: "ham",
+    img: './images/ham2.jpg'
+},
+{
+    id: 6,
+    name: "Margarita",
+    desc: "Masa delgada, albahaca, rodajas de tomate y queso mozzarella fresco.",
+    type: "pizza",
+    img: './images/pizza1.jpg'
+},
+{
+    id: 7,
+    name: "Caprichosa",
+    desc: "La mejor de todas, pídela y no te arrepentirás. Con todo.",
+    type: "pizza",
+    img: './images/pizza2.jpg'
+},
+{
+    id: 8,
+    name: "Clásica",
+    desc: "Nada como la pizza de Pepperoni. La nuestra es la mejor de todas.",
+    type: "pizza",
+    img: './images/pizza3.jpg'
+},
+{
+    id: 9,
+    name: "Fresa",
+    desc: "Smoothie sabroso de fresa con leche de almendras.",
+    type: "shake",
+    img: './images/shake1.jpg'
+},
+{
+    id: 10,
+    name: "Mora",
+    desc: "Smoothie sabroso de mora con leche de almendras o agua.",
+    type: "shake",
+    img: './images/shake2.jpg'
+},
+{
+    id: 11,
+    name: "Tropical",
+    desc: "Smoothie sabroso de frutas con leche de almendras o agua.",
+    type: "shake",
+    img: './images/shake3.jpg'
+},
+{
+    id: 12,
+    name: "Piraña",
+    desc: "Tacos como los comimos en Puebla en el 2019, los mejores.",
+    type: "taco",
+    img: './images/tacos1.jpg'
+},
+{
+    id: 13,
+    name: "Morelia",
+    desc: "Casada con frijoles y acompañamientos típicos mexicanos.",
+    type: "taco",
+    img: './images/tacos2.jpg'
+},
+{
+    id: 14,
+    name: "Chocolate Cake",
+    desc: "Hermoso queque de chocolate que acompañará la mejor comida. Pídelo con extra chantillí.",
+    type: "postre",
+    img: './images/postre1.jpg'
+},
+{
+    id: 15,
+    name: "Fluff",
+    desc: "Con este postre vas a querer repetir. Para cerrar con broche de oro",
+    type: "postre",
+    img: './images/postre2.jpg'
+},
+]
+
+
+export default food
---
+++ menu/index.js
@@ -0,0 +1,39 @@
+import food from './data.js'
+
+const menu = document.querySelector('.menu')
+const menuBtns = document.querySelectorAll('.menu-btns')
+
+menuBtns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        const option = e.target.dataset.id
+        if (option === 'all') {
+            return displayOnScreen(food)
+        }
+        const menuFiltered = food.filter(meal => {
+            if (meal.type === option) {
+                return meal
+            }
+        })
+        displayOnScreen(menuFiltered)
+    })
+})
+
+
+function displayOnScreen(food) {
+    let data = food.map(meal => {
+        return `<div class="card" style="width: 18rem">
+        <div class="card-img-top">
+          <img src="${meal.img}" alt="" class="food-img"/>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${meal.name}</h5>
+          <p class="card-text">
+            ${meal.desc}  
+          </p>
+          <a href="#" class="btn btn-primary form-control">Buy</a>
+        </div>
+        </div>`
+    })
+
+    menu.innerHTML = data.join("")
+}
